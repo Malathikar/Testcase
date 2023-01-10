c9361fa3669459d0ac6db907063f69df9539b82b
Malathikar <120227113+Malathikar@users.noreply.github.com>
2023-01-10 11:36:06 -0700

    update

--- page-objects/mcdonald.js
+++ page-objects/mcdonald.js
@@ -1,27 +1,18 @@
 //const helpers = require("../runtime/helpers");
 module.exports = {
-
     url: 'http://www.mcdonalds.com/',
-
     elements: {
         MyMcdonaldsreward: '//*[@id="container-1e52aa8d39"]/div/div/div/div/div[2]/div/div[2]/div/nav/ul/li[3]/a',
-       
         MyMcdonald: '//*[@id="teaser-22e451c16e"]/div/div/div[1]/h2'
-  
     },
-    clickElement: async function(objectKey) {
+    clickElement: async function (objectKey) {
         var selector = page.mcdonald.elements[objectKey];
         await driver.sleep(5000);
         return driver.findElement(By.xpath(selector)).click();
     },
-      elementExists: async function(objectKey) {
-        
+    elementExists: async function (objectKey) {
         var selector = page.mcdonald.elements[objectKey];
-
         await driver.sleep(5000);
-      
         return driver.findElement(By.xpath(selector));
     },
-  
-
 };
